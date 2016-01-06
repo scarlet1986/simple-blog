016d517c9f365aa37d434ac4efaf2e8c4c778c5f
scarlet1986 <scarlet1986@ukr.net>
2016-01-06 14:39:07 +0200

    views and controller for posts

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,43 @@
+class Admin::PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+    redirect_to [:admin, @post]
+    else
+     render 'new'
+    end
+  end
+  def index
+    @posts = Post.all
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to [:admin, @post]
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to [:admin, :posts]
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  resources :posts, only: [:index, :show]
+
+  namespace :admin do
+    resources :posts
+  end
   devise_for :admins
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
