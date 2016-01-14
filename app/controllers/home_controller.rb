626bde07c255fe586fc6ee03a151ed35da4f7a80
scarlet1986 <scarlet1986@ukr.net>
2016-01-14 19:05:08 +0200

    delete pre-tag, add heading, some styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.limit(5).order(id: :desc)
-    @news_items = NewsItem.limit(7).order(id: :desc)
+    @news_items = NewsItem.limit(5).order(id: :desc)
   end
 end
